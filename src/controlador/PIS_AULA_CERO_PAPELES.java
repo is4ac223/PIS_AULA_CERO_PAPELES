97a4f5b1b430c9e7ff18ffcb474f9472e2d214c0
mrb1ngus <137521933+mrb1ngus@users.noreply.github.com>
2023-11-27 01:14:23 -0500

    Commit Leo Cambie el nombre del paquete main por controlador Cambie el archivo main a controlador

--- src/controlador/PIS_AULA_CERO_PAPELES.java
+++ src/controlador/PIS_AULA_CERO_PAPELES.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package pis_aula_cero_papeles;
+package controlador;
 
 /**
  *
