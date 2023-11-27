00652d15ba85492be7e1f70ce8a14f7b7ca4c00b
mrb1ngus <137521933+mrb1ngus@users.noreply.github.com>
2023-11-27 00:32:40 -0500

    Carga inicial

---
+++ src/pis_aula_cero_papeles/PIS_AULA_CERO_PAPELES.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pis_aula_cero_papeles;
+
+/**
+ *
+ * @author leona
+ */
+public class PIS_AULA_CERO_PAPELES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
